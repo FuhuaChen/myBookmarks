8a7fa7b6d91af5dcd66a5a72c2482714d6865940
FuhuaChen <874166712@qq.com>
2017-11-18 10:47:57 +0800

    添加三个文件

---
+++ main.js
@@ -0,0 +1,68 @@
+var keys = {
+    0: ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
+    1: ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
+    2: ['z', 'x', 'c', 'v', 'b', 'n', 'm'],
+    length: 3
+}
+var hash = {
+    'q': 'qq.com',
+    'w': 'weibo.com',
+    'e': 'explainshell.com',
+    'r': 'renren.com',
+    't': 'tianya.com',
+    'y': 'youtube.com',
+    'u': 'uc.com',
+    'i': 'iconfont.cn',
+    'o': 'opera.com',
+    'p': 'processon.com',
+    'a': 'alpha.wallhaven.cc',
+    's': 'sohu.com',
+    'd': 'dribbble.com',
+    'g': 'github.com',
+    'j': 'javascript.ruanyifeng.com',
+    'z': 'zhihu.com',
+    'c': 'colorzilla.com',
+    'v': 'visualgo.net',
+    'n': 'namesilo.com',
+    'm': 'mcdonalds.com.cn'
+}
+//取出localStorage ‘zzz’ 里对应的hash
+var hashInlocalStorage = JSON.parse(localStorage.getItem('zzz') || 'null')
+//如果hashInlocaStorage存在就覆盖
+if (hashInlocalStorage) {
+    hash = hashInlocalStorage
+}
+//遍历数组，生成kbd、div标签，插入标签
+var index = 0
+while (index < keys.length) { //取值为0 1 2
+    div1 = document.createElement('div')
+    main.appendChild(div1)
+    row = keys[index]  //取值为 第一个数组 第二个数组 第三个数组
+    index2 = 0
+    while (index2 < row.length) { //取值为 0-9 0-8 0-7
+        kbd1 = document.createElement('kbd')
+        kbd1.textContent = row[index2]
+        button1 = document.createElement('button')
+        button1.textContent = '编辑'
+        kbd1.appendChild(button1)
+        button1.id = row[index2]  //给button一个id
+        //other[target]就是用户点击的元素
+        button1.onclick = function (other) {
+            key = other.target.id  //用户点击的元素的id
+            x = prompt('请输入一个网址')
+            hash[key] = x  //hash变更
+            //把变更的hash存入localStorage
+            localStorage.setItem('zzz', JSON.stringify(hash))
+            console.log(hash)
+        }
+        div1.appendChild(kbd1)
+        index2 = index2 + 1
+    }
+    index = index + 1
+}
+document.onkeypress = function (other) {
+    key = other.key  //取用户输入的键
+    website = hash[key] //取hash里对应的网址
+    location.href = 'http://' + website
+    //window.open('http://' + website, '_blank')  在新窗口打开
+}
